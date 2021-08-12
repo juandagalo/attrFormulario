54cfdfe9f768ff35ac4927b08eafd5daf44f2e77
Juan Daniel Garcia Lopez <juandagarcial@gmail.com>
2021-08-12 16:57:41 -0500

    Estilos de SIam en Home
    Se agregan los estilos de SIAM en la pantalla de HOME

--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { FormularioMlfhService } from '../../services/formulario-mlfh.service';
 
+
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
@@ -21,16 +22,16 @@ export class FormularioComponent implements OnInit {
   formularioMlfh:any                                  = {};
   
 
-  constructor(private forumlarioMLFHService:FormularioMlfhService,
-              private router:ActivatedRoute) { 
+  constructor(private forumlarioMLFHService: FormularioMlfhService,
+              private router: ActivatedRoute) {
 
       this.router.params.subscribe(params =>{
-  
+
         this.getFormulario();
-        
-      })
+      });
   }
 
+  // tslint:disable-next-line: typedef
   getFormulario(){
     this.formularioMlfh = this.forumlarioMLFHService.getforumlariomlfh();
   }
@@ -38,28 +39,29 @@ export class FormularioComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  mostrarDispositivosCardiacos(implanteCardiaco:boolean){
+  // tslint:disable-next-line: typedef
+  mostrarDispositivosCardiacos(implanteCardiaco: boolean){
 
       this.implanteCardiaco = implanteCardiaco;
-      
   }
 
-  otroSindromeClinico(tieneOtroSindromeClinico:boolean){
+  otroSindromeClinico(tieneOtroSindromeClinico:boolean): void{
 
     this.tieneOtroSindromeClinico = tieneOtroSindromeClinico;
 
   }
 
+  // tslint:disable-next-line: typedef
   mostrarManifestacionesExtracardiacas(tieneManifestacionesExtracardiacas:boolean){
 
     this.tieneManifestacionesExtracardiacas = tieneManifestacionesExtracardiacas;
 
-    if (!tieneManifestacionesExtracardiacas) this.tieneOtrasManifestacionesExtracardiacas = tieneManifestacionesExtracardiacas;
- 
+    if (!tieneManifestacionesExtracardiacas) { this.tieneOtrasManifestacionesExtracardiacas = tieneManifestacionesExtracardiacas; }
 
   }
 
 
+  // tslint:disable-next-line: typedef
   otrasManifestacionesExtracardiacas(tieneOtrasManifestacionesExtracardiacas:boolean){
 
     this.tieneOtrasManifestacionesExtracardiacas = tieneOtrasManifestacionesExtracardiacas;
@@ -67,22 +69,27 @@ export class FormularioComponent implements OnInit {
   }
 
 
+  // tslint:disable-next-line: typedef
   mostrarGrosorVentriculoIzquierdo(tieneGrosorVentriculoIzquierdo:boolean){
     this.tieneGrosorVentriculoIzquierdo = tieneGrosorVentriculoIzquierdo;
   }
 
+  // tslint:disable-next-line: typedef
   mostrarResonanciaMagnetica(tieneResonanciaMagnetica:boolean){
     this.tieneResonanciaMagnetica = tieneResonanciaMagnetica;
   }
 
+  // tslint:disable-next-line: typedef
   mostrarAttr(tieneAtter:boolean){
     this.tieneAtter = tieneAtter;
   }
 
+  // tslint:disable-next-line: typedef
   otraInvervencionFarmacologica(tieneOtraInvervencionFarmacologica:boolean){
     this.tieneOtraInvervencionFarmacologica = tieneOtraInvervencionFarmacologica;
   }
 
+  // tslint:disable-next-line: typedef
   mostrarHospitalizacionesFCardiaca(tieneHospitalizacionesFCardiaca:boolean){
     this.tieneHospitalizacionesFCardiaca = tieneHospitalizacionesFCardiaca;
   }
